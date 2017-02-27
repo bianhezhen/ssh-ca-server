348cf157b0b707f26d3f93f7f0510ddb282f731b
Dave Pigliavento <dpigliavento@commercehub.com>
2017-02-27 15:39:23 -0500

    flask does not support 3.2

--- setup.py
+++ setup.py
@@ -31,12 +31,9 @@ setup(
 
     classifiers=[
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.5'
     ],
     install_requires=[
         'ldap3>=2.2.1',
